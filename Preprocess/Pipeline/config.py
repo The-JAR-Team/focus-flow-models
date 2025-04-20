0d34a388ac5d7ba78704d20257be2ed2eea2f4e4
Jonathan Shaya <jonathansh2@mta.ac.il>
2025-04-21 00:02:06 +0300

    -run.py hot fix

--- Preprocess/Pipeline/config.py
+++
@@ -1,3 +0,0 @@
-DAISEE_DATASET_ROOT = r"C:\Users\bhbha\OneDrive - The Academic College of Tel-Aviv Jaffa - MTA\Desktop\dataset\DaiseeData\DataSet"  # The folder with Train/, Test/, Validation/
-ENGAGENET_DATASET_ROOT = r"C:\Users\bhbha\OneDrive - The Academic College of Tel-Aviv Jaffa - MTA\Desktop\dataset\MARLIN"  # The folder with Train/, Test/, Validation/
-CACHE_DIR = r"C:\Users\bhbha\OneDrive - The Academic College of Tel-Aviv Jaffa - MTA\Desktop\dataset\Cache"
