ce54c622457dd08d723b018ec4895ba6a76b148c
Jonathan Shaya <jonathansh2@mta.ac.il>
2025-04-23 10:15:41 +0300

    -New 80 20 dist configration

---
+++ Preprocess/Pipeline/example_config.py
@@ -0,0 +1,3 @@
+DAISEE_DATASET_ROOT = r"path_to\dataset\DaiseeData\DataSet"  # The folder with Train/, Test/, Validation/
+ENGAGENET_DATASET_ROOT = r"path_to\dataset\MARLIN"  # The folder with Train/, Test/, Validation/
+CACHE_DIR = r"path_to\dataset\Cache"
